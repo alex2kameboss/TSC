726099fa95267d0d24fe3a7008bc42839e0d5fa7
alex2kameboss <palexandru2000@gmail.com>
2023-03-07 13:34:28 +0200

    add lab3 exs

---
+++ tsc_lab3/lab_dut/instr_register_pkg.sv
@@ -0,0 +1,29 @@
+/***********************************************************************
+ * A SystemVerilog RTL model of an instruction regisgter:
+ * User-defined type definitions
+ **********************************************************************/
+`timescale 1ns/1ns
+package instr_register_pkg;
+
+  typedef enum logic [3:0] {
+  	ZERO,
+    PASSA,
+    PASSB,
+    ADD,
+    SUB,
+    MULT,
+    DIV,
+    MOD
+  } opcode_t;
+
+  typedef logic signed [31:0] operand_t;
+  
+  typedef logic [4:0] address_t;
+  
+  typedef struct {
+    opcode_t  opc;
+    operand_t op_a;
+    operand_t op_b;
+  } instruction_t;
+
+endpackage: instr_register_pkg
