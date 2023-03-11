9feba76883f6d3639a379731ff46529d03cf331f
alex2kameboss <palexandru2000@gmail.com>
2023-03-11 09:09:38 +0200

    compute alu result

--- tsc_lab3/lab_dut/instr_register.sv
+++ tsc_lab3/lab_dut/instr_register.sv
@@ -19,16 +19,41 @@ import instr_register_pkg::*;  // user-defined types are defined in instr_regist
  output instruction_t  instruction_word
 );
   instruction_t  iw_reg [0:31];  // an array of instruction_word structures
+  result_t result, ext_a, ext_b;
+  operand_t intermediar;
 
   // write to the register
-  always@(posedge clk, negedge reset_n)   // write into register
+  always_ff @(posedge clk, negedge reset_n)   // write into register
     if (!reset_n) begin
       foreach (iw_reg[i])
         iw_reg[i] = '{opc:ZERO,default:0};  // reset to all zeros
     end
     else if (load_en) begin
-      iw_reg[write_pointer] = '{opcode,operand_a,operand_b};
+      iw_reg[write_pointer] = '{opcode, operand_a, operand_b, result};
     end
+
+  assign ext_a = {{32{operand_a[31]}}, operand_a};
+  assign ext_b = {{32{operand_b[31]}}, operand_b};
+
+  always_comb begin
+    result = 0;
+    unique case (opcode)
+      ZERO: result = 0;
+      PASSA: result = ext_a;
+      PASSB: result = ext_b;
+      ADD: result = ext_a + ext_b;
+      SUB: result = ext_a - ext_b;
+      MULT: result = operand_a * operand_b;
+      DIV: begin
+        intermediar = operand_a / operand_b;
+        result = {{31{intermediar[31]}}, intermediar};
+      end
+      MOD: begin
+        intermediar = operand_a % operand_b;
+        result = {{31{intermediar[31]}}, intermediar};
+      end
+    endcase
+  end
 
   // read from the register
   assign instruction_word = iw_reg[read_pointer];  // continuously read from register
